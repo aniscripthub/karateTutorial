5c36eb16a88603e6664ee7af9be8ee569650f472
AniScript Hub <ankitsingh6497@gmail.com>
2025-03-20 17:46:13 +0530

    testRunner file

---
+++ learnKarate/src/test/java/com/example/tests/KarateTestRunner.java
@@ -0,0 +1,9 @@
+import com.intuit.karate.junit5.Karate;
+
+class KarateTestRunner {
+
+    @Karate.Test
+    Karate testAll() {
+        return Karate.run("classpath:").relativeTo(getClass());
+    }
+}
